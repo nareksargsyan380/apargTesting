fa0d1c0ce261bddfe18f8000c40df8db889c676d
Narek <sargsyannarek380@gmail.com>
2019-05-03 03:35:51 +0400

    show photos by searching

--- files/js/js.js
+++ files/js/js.js
@@ -1,44 +1,56 @@
-$(function bentley(){
-  //JSON, .getJSON()
-  var flickApiUrl = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"
+// getting photos from flickr
+function someFunc(){
+if(document.getElementById("myInput").value == "mercedes"){
+      alert("Mercedes")
+      $(function mercedes(){
+        //JSON, .getJSON()
+        var flickApiUrl = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"
 
-  $.getJSON(flickApiUrl, {
-    tags: "bentley",
-    tagmode: "any",
-    format: "json"
-  }).done(function(data) {
-    console.log(data);
-    $.each(data.items, function(index, item) {
-      console.log(item);
-      $("<img>").attr("src", item.media.m).appendTo("#flickr");
+        $.getJSON(flickApiUrl, {
+          tags: "mercedesw221, w222",
+          tagmode: "any",
+          format: "json"
+        }).done(function(data) {
+          console.log(data);
+          $.each(data.items, function(index, item) {
+            console.log(item);
+            $("<img>").attr("src", item.media.m).appendTo(".merc");
 
-      if(index == 4) {
-        return false;
-      }
-    });
-  }).fail(function(){
-    alert("Ajax call failed.");
-  });
-});
-$(function mercedes(){
-  //JSON, .getJSON()
-  var flickApiUrl = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"
+            if(index == 4) {
+              return false;
+            }
+          });
+        }).fail(function(){
+          alert("Ajax call failed.");
+        });
+      });
+}else if (document.getElementById("myInput").value == "bentley") {
+      alert("Bentley")
+      $(function bentley(){
+        //JSON, .getJSON()
+        var flickApiUrl = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?"
 
-  $.getJSON(flickApiUrl, {
-    tags: "mercedesw221, w222",
-    tagmode: "any",
-    format: "json"
-  }).done(function(data) {
-    console.log(data);
-    $.each(data.items, function(index, item) {
-      console.log(item);
-      $("<img>").attr("src", item.media.m).appendTo("#flickr");
+        $.getJSON(flickApiUrl, {
+          tags: "bentley",
+          tagmode: "any",
+          format: "json"
+        }).done(function(data) {
+          console.log(data);
+          $.each(data.items, function(index, item) {
+            console.log(item);
+            $("<img>").attr("src", item.media.m).appendTo(".bent");
 
-      if(index == 4) {
-        return false;
-      }
-    });
-  }).fail(function(){
-    alert("Ajax call failed.");
-  });
-});
+            if(index == 4) {
+              return false;
+            }
+          });
+        }).fail(function(){
+          alert("Ajax call failed.");
+        });
+      });
+}else{
+  alert("Please enter mercedes or bentley with lowercase")};
+}
+
+// search clicking
+document.getElementById("searchBtn").onclick = someFunc;
